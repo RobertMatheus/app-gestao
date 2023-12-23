1b945c71bf21bee67338348461d5245ff4f4866f
RobertMatheus <matheusrobert531@gmail.com>
2023-12-23 18:04:08 -0300

    atualizaçã pendente

---
+++ resources/views/site/templates/template_1.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Super Gestão - Sobre Nós</title>
+        <meta charset="utf-8">
+        <link  rel="stylesheet" href="{{asset('css\app.css')}}">
+        
+    </head>
+
+    <body>
+        
+        @yield('meio')
+
+    </body>
+</html>
